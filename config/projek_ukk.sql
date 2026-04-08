4a3ed2bb540b8568ca40797b9cbe5bdc15e67c8d
fazmiraffi2 <fazmiraffi2@gmail.com>
2026-04-08 09:29:27 +0700

    update

--- config/projek_ukk.sql
+++ config/projek_ukk.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Mar 31, 2026 at 06:47 AM
+-- Generation Time: Apr 08, 2026 at 02:26 AM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -76,7 +76,66 @@ INSERT INTO `buku` (`id`, `judul`, `penulis`, `kelas_buku`, `stok`, `foto`, `cre
 (32, 'Si Tudung Merah', 'Ika Setyowati', 'Cerita', 4, '699850f09840a.jpg', '2026-02-20 12:17:52'),
 (33, 'Kisah Bumi', 'Riski Dian Permana', 'Cerita', 3, '6998513799c31.jpg', '2026-02-20 12:19:03'),
 (34, 'Kisah Tiga Saudara', 'Gtot Harmanto', 'Cerita', 14, '6998518341db3.jpg', '2026-02-20 12:20:19'),
-(35, 'Pada Zaman Dahulu ', 'Nunung Nurhayanti', 'Cerita', 7, '699851fc45e73.jpg', '2026-02-20 12:22:20');
+(35, 'Pada Zaman Dahulu ', 'Nunung Nurhayanti', 'Cerita', 8, '699851fc45e73.jpg', '2026-02-20 12:22:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  `buku_id` int DEFAULT NULL,
+  `tanggal_pinjam` date DEFAULT NULL,
+  `tanggal_kembali` date DEFAULT NULL,
+  `tanggal_kembali_aktual` date DEFAULT NULL,
+  `denda` int DEFAULT '0',
+  `status` enum('pending','dipinjam','proses_kembali','kembali','ditolak') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id`, `user_id`, `buku_id`, `tanggal_pinjam`, `tanggal_kembali`, `tanggal_kembali_aktual`, `denda`, `status`) VALUES
+(14, 9, 35, '2026-02-20', '2026-02-27', '2026-03-11', 12000, 'kembali'),
+(15, 9, 13, '2026-02-22', '2026-03-01', '2026-03-11', 10000, 'kembali'),
+(16, 9, 34, '2026-03-09', '2026-03-16', '2026-03-11', 0, 'kembali'),
+(17, 9, 2, '2026-03-11', '2026-03-18', '2026-03-11', 0, 'kembali'),
+(18, 9, 33, '2026-03-11', '2026-03-18', NULL, 0, 'pending'),
+(19, 9, 35, '2026-04-06', '2026-04-13', '2026-04-06', 0, 'kembali'),
+(20, 9, 34, '2026-04-06', '2026-04-13', NULL, 0, 'pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nama_lengkap` varchar(100) DEFAULT NULL,
+  `kelas` varchar(20) DEFAULT NULL,
+  `jurusan` varchar(50) DEFAULT NULL,
+  `role` enum('admin','siswa') DEFAULT 'siswa',
+  `foto` varchar(255) DEFAULT 'default.png'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `nama_lengkap`, `kelas`, `jurusan`, `role`, `foto`) VALUES
+(1, 'admin1', 'admin123', 'Administrator Utama', '-', '-', 'admin', 'default.png'),
+(2, 'siswa1', 'siswa123', 'dafa', 'XII', 'RPL 1', 'siswa', 'default.png'),
+(9, 'Piww', '123', 'Raffi Ramadhan Fazmi', 'XII', 'Rekayasa Perangkat Lunak', 'siswa', 'default.png'),
+(10, 'a', 'a', 'a', 'X', 'RPL 1', 'siswa', 'default.png'),
+(11, 'b', 'b', 'b', 'X', 'RPL 1', 'siswa', 'default.png'),
+(12, '830julianr', '20217142', 'Julian Ricardo', 'XII', 'RPL 4', 'siswa', 'default.png');
 
 --
 -- Indexes for dumped tables
@@ -89,6 +148,20 @@ ALTER TABLE `buku`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `peminjaman_ibfk_1` (`user_id`),
+  ADD KEY `peminjaman_ibfk_2` (`buku_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -97,6 +170,29 @@ ALTER TABLE `buku`
 --
 ALTER TABLE `buku`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`buku_id`) REFERENCES `buku` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
